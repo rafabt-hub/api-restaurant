555a4a7a8d6379b0a23966a4b1a1aed50069bc26
Rafael <rafaelbthomaz@gmail.com>
2025-03-31 14:56:43 -0300

    adding sum routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,13 @@ import { Router } from "express"
 import { tablesSessionsRoutes } from "./tables-sessions-routes"
 import { productsRoutes } from "./products-routes"
 import { tablesRoutes } from "./tables-routes"
+import { ordersRoutes } from "./orders-routes"
 
 const routes = Router()
 routes.use("/tables-sessions", tablesSessionsRoutes)
 routes.use("/products", productsRoutes)
 routes.use("/tables", tablesRoutes)
+routes.use("/orders", ordersRoutes)
+
 
 export { routes }

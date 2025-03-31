2b70d2b80ae1fe15e11dd5bbbe5ed41b3c6ea9c0
Rafael <rafaelbthomaz@gmail.com>
2025-03-31 12:05:34 -0300

    adding routes to products and errors

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -5,6 +5,9 @@ const productsRoutes = Router()
 const productController = new ProductController()
 
 productsRoutes.get("/", productController.index)
+productsRoutes.post("/", productController.create)
+productsRoutes.put("/:id", productController.update)
+productsRoutes.delete("/:id", productController.remove)
 
 export { productsRoutes }
 
